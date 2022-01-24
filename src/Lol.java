f7c8adc7a920edacb3a0c503167400754a1776d6
Daniel_J <frostysausage123@gmail.com>
2022-01-24 11:59:26 -0600

    master commit

--- src/Lol.java
+++ src/Lol.java
@@ -2,5 +2,6 @@ public class Lol {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("tested");
+        System.out.println("tested2");
     }
 }
